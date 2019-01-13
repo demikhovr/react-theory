eace5e7e5e76822132a5523369b02de896ea5852
demikhovr <demikhovr@gmail.com>
2019-01-13 14:27:22 +0300

    Updates

---
+++ precache-manifest.cb2af521a1d7f8483ef186372c178e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d62adf9f316ed59e74d",
+    "url": "/react-theory/static/js/runtime~main.9d62adf9.js"
+  },
+  {
+    "revision": "1fc7c3756eb392977035",
+    "url": "/react-theory/static/js/main.1fc7c375.chunk.js"
+  },
+  {
+    "revision": "c7d5fa902d8b277fce9f",
+    "url": "/react-theory/static/js/1.c7d5fa90.chunk.js"
+  },
+  {
+    "revision": "1fc7c3756eb392977035",
+    "url": "/react-theory/static/css/main.89f9d323.chunk.css"
+  },
+  {
+    "revision": "2b6bff6ebd658201f28eb9f4f32aeaf4",
+    "url": "/react-theory/index.html"
+  }
+];
